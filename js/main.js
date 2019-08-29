cf36aa8aa29f2105b76ac0a2f3826b87b4a988d2
MD Riaz <riazmd582@gmail.com>
2019-08-29 21:30:00 +0600

    GRID IMG CHANGE

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 /* Shortcode */
 var query = document.querySelector.bind(document);
+var queryAll = document.querySelectorAll.bind(document);
 /* 
 variables
 */
 let navBtn = query(".nav");
 let closeBtn = query(".close__btn")
 let navlinks = query(".nav__links");
-
+let btn_tabs = queryAll(".btn__tab");
 /*
 events
 */
 navBtn.addEventListener("click", () => navlinks.classList.add("active"));
 closeBtn.addEventListener("click", () => navlinks.classList.remove("active"));
+
+btn_tabs.forEach(el => {
+    el.addEventListener("click", () => {
+        btn_tabs.forEach( tab => tab.classList.remove("active"));
+        el.classList.add("active");
+    });
+});
